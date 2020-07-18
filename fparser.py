19eeadd2ea142709d31f7c702ec2836974051a4c
Brian Windrim <bwindrim@gmail.com>
2020-07-18 19:34:25 +0100

    - now use body_start and body_end positions, everything in absolute tell() values

--- fparser.py
+++ fparser.py
@@ -27,12 +27,17 @@ def get_atom_head(f):
     return atom_size, atom_type
 
 
-def process_ftyp(f, atom_size):
+def process_ftyp(f, body_end):
     "process the body of an ftyp atom"
-    magic2 = f.read(4)                   # read bytes 8..11
-    atom_size -= 4
-    print ("magic2 = ", magic2)
+    type_list = []
 
+    while (f.tell() < body_end):
+        magic2 = f.read(4)                   # read bytes 8..11
+        print ("magic2 = ", magic2)
+        type_list.append(magic2)
+
+    magic2 = type_list[0]
+    
     if (magic2 == b'isom'):
         print('It is an ISO Base Media file (MPEG-4) v1')
     elif (magic2 == b'MSNV'):
@@ -46,35 +51,39 @@ def process_ftyp(f, atom_size):
     else:
         print('Unknown ftyp', magic2)
 
-    print("seeking by", atom_size)
-    f.seek(atom_size, 1) # skip the rest of the atom body
+    print("seeking to", body_end)
+    f.seek(body_end, 0) # skip the rest of the atom body
         
-    return atom_size
+    return type_list
 
 
-def process_mdat(f, atom_size):
+def process_mdat(f, body_end):
     "process the body of an ftyp atom"
 
-    print("mdat atom, seeking by", atom_size)
-    f.seek(atom_size, 1) # skip the rest of the atom body
+    print("mdat atom, seeking to", body_end)
+    body_start = f.tell()
+    f.seek(body_end, 0) # skip the rest of the atom body
         
-    return atom_size
+    return [body_start, body_end]
 
 
-def process_moov(f, atom_size):
+def process_moov(f, body_end):
     "process the body of an ftyp atom"
 
-    print("moov atom, seeking by", atom_size)
-    f.seek(atom_size, 1) # skip the rest of the atom body
+    print("moov atom, seeking to", body_end)
+    f.seek(body_end, 0) # skip the rest of the atom body
         
-    return atom_size
+    return []
 
 
-def process_next_atom(f, length):
+def process_next_atom(f, end_pos):
     "read the next atom from file f and process it"
     
     atom_size, atom_type = get_atom_head(f)
 
+    atom_body = None
+    pos = f.tell()
+    
     if (atom_type == b'wide'):
         print("ignoring wide atom, atom body size = ", atom_size)
         assert(0 == atom_size)
@@ -85,13 +94,15 @@ def process_next_atom(f, length):
         print("free atom, seeking by ", atom_size)
         f.seek(atom_size, 1) # skip to the atom_size (wrt. file head)
     elif (atom_type == b'ftyp'):
-        atom_size = process_ftyp(f, atom_size)
+        atom_body = process_ftyp(f, pos + atom_size)
     elif (atom_type == b'mdat'):
-        atom_size = process_mdat(f, atom_size)
+        atom_body = process_mdat(f, pos + atom_size)
     elif (atom_type == b'moov'):
-        atom_size = process_moov(f, atom_size)
+        atom_body = process_moov(f, pos + atom_size)
     else:
         print('Unrecognised atom type', atom_type)
+
+    return [atom_type, atom_size, atom_body]
     
 
 file_list = sys.argv        # get the argument list
@@ -101,12 +112,16 @@ print("prog_name = ", prog_name)
 print("file_list = ", file_list)
 
 for pathname in file_list:
+    atom_list = []
     file_length = os.stat(pathname).st_size
     print("pathname = ", pathname)
     with open(pathname, 'rb') as f:
         while (f.tell() < file_length):
-            process_next_atom(f, file_length)
+            atom_list.append (process_next_atom(f, file_length))
             print("file offset = ", f.tell())
+
+    print("Atom_list = ", atom_list)
+    
         
 
 

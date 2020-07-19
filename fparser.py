7a6abc11c8663b6faac927a2f85b97333e862109
Brian Windrim <bwindrim@gmail.com>
2020-07-19 13:46:00 +0100

    - removed get_atom_head() - added ecplicit returns for all handlers

--- fparser.py
+++ fparser.py
@@ -1,29 +1,6 @@
 import sys
 import os
 
-def get_atom_head(f):
-    "Get the next atom from the file"
-    data = f.read(4)                    # read the first bytes 0..3
-    atom_size = int.from_bytes(data,"big") # convert 32 bits to integer
-    atom_type = f.read(4)                   # read bytes 4..7
-
-    if (1 == atom_size):
-        # using extended size field
-        print("using extended atom size")
-        data = f.read(8)
-        atom_size = int.from_bytes(data,"big") # convert 64 bits to integer
-    
-    # todo: handle special size cases, 0 and 1
-    print("get_atom_head() data = ", data,
-          "atom_size = ", atom_size,
-          "atom_type = ", atom_type)
-
-    print ("atom_size =", atom_size)
-    assert (atom_size >= 0)
-
-    return atom_size, atom_type
-
-
 def process_ftyp(f, body_end):
     "process the body of an ftyp atom"
     type_list = []
@@ -55,17 +32,17 @@ def process_ftyp(f, body_end):
 
 
 def process_mdat(f, body_end):
-    "process the body of an ftyp atom"
+    "process the body of an mdat atom"
 
-    print("mdat atom, seeking to", body_end)
     body_start = f.tell()
+    print("mdat atom, seeking to", body_end)
     f.seek(body_end, 0) # skip the rest of the atom body
         
     return [body_start, body_end]
 
 
 def process_moov(f, body_end):
-    "process the body of an ftyp atom"
+    "process the body of a moov atom"
 
     print("moov atom, seeking to", body_end)
     f.seek(body_end, 0) # skip the rest of the atom body
@@ -78,23 +55,31 @@ def process_wide(f, body_end):
     print("ignoring wide atom")
     assert(body_end == f.tell())
 
+    return None
+
 
 def process_skip(f, body_end):
     "process the body of an skip atom"
     print("skip atom, seeking to ", body_end)
     f.seek(body_end, 0) # skip to the atom_size (wrt. file head)
 
+    return body_end
 
 def process_free(f, body_end):
     "process the body of a free atom"
     print("free atom, seeking to ", body_end)
     f.seek(body_end, 0) # skip to the atom_size (wrt. file head)
+    # todo: check body for consisten value at return [value, byte_count]
+    return body_end
 
 
 def process_unknown(f, body_end):
     "process the body of an unknown atom"
+    body_start = f.tell()
     print("unknown atom, seeking to ", body_end)
     f.seek(body_end, 0) # skip to the atom_size (wrt. file head)
+
+    return [body_start, body_end]
 
 
 # Mapping dict from atom type code to handler function
@@ -157,15 +142,18 @@ prog_name = file_list.pop(0) # pop the script name off the head of the list
 print("prog_name = ", prog_name)
 print("file_list = ", file_list)
 
+# Loop over the files from the command line
 for pathname in file_list:
     atom_list = []
     file_length = os.stat(pathname).st_size
     print("pathname = ", pathname)
     with open(pathname, 'rb') as f:
+        # Loop over the atoms in the file
         while (f.tell() < file_length):
             atom_list.append (process_next_atom(f, file_length))
-            print("file offset = ", f.tell())
+#            print("file offset = ", f.tell())
 
+    # We've been through the file, now print the list of atoms
     print("Atom_list = ", atom_list)
     
         

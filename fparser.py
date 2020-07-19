c4fc8af9a060adaf5b2b4cec96cdfaf3920cefec
Brian Windrim <bwindrim@gmail.com>
2020-07-19 14:17:32 +0100

    - renamed process_ functions to get_ - moved ftype checking to is_valid_file()

--- fparser.py
+++ fparser.py
@@ -1,7 +1,7 @@
 import sys
 import os
 
-def process_ftyp(f, body_end):
+def get_ftyp(f, body_end):
     "process the body of an ftyp atom"
     type_list = []
 
@@ -10,28 +10,13 @@ def process_ftyp(f, body_end):
         print ("magic2 = ", magic2)
         type_list.append(magic2)
 
-    magic2 = type_list[0]
-    
-    if (magic2 == b'isom'):
-        print('It is an ISO Base Media file (MPEG-4) v1')
-    elif (magic2 == b'MSNV'):
-        print('It is a MPEG-4 video file')
-    elif (magic2 == b'mp42'):
-        print('It is an MPEG-4 video|QuickTime file')
-    elif (magic2 == b'qt  '):
-        print('It is a QuickTime movie file')
-    elif (magic2 == b'M4V '):
-        print('It is an ISO Media, MPEG v4 system, or iTunes AVC-LC file')
-    else:
-        print('Unknown ftyp', magic2)
-
     print("seeking to", body_end)
     f.seek(body_end, 0) # skip the rest of the atom body
         
     return type_list
 
 
-def process_mdat(f, body_end):
+def get_mdat(f, body_end):
     "process the body of an mdat atom"
 
     body_start = f.tell()
@@ -41,7 +26,7 @@ def process_mdat(f, body_end):
     return [body_start, body_end]
 
 
-def process_moov(f, body_end):
+def get_moov(f, body_end):
     "process the body of a moov atom"
 
     print("moov atom, seeking to", body_end)
@@ -50,7 +35,7 @@ def process_moov(f, body_end):
     return []
 
 
-def process_wide(f, body_end):
+def get_wide(f, body_end):
     "process the body of a wide atom"
     print("ignoring wide atom")
     assert(body_end == f.tell())
@@ -58,14 +43,14 @@ def process_wide(f, body_end):
     return None
 
 
-def process_skip(f, body_end):
+def get_skip(f, body_end):
     "process the body of an skip atom"
     print("skip atom, seeking to ", body_end)
     f.seek(body_end, 0) # skip to the atom_size (wrt. file head)
 
     return body_end
 
-def process_free(f, body_end):
+def get_free(f, body_end):
     "process the body of a free atom"
     print("free atom, seeking to ", body_end)
     f.seek(body_end, 0) # skip to the atom_size (wrt. file head)
@@ -73,7 +58,7 @@ def process_free(f, body_end):
     return body_end
 
 
-def process_unknown(f, body_end):
+def get_unknown(f, body_end):
     "process the body of an unknown atom"
     body_start = f.tell()
     print("unknown atom, seeking to ", body_end)
@@ -84,16 +69,16 @@ def process_unknown(f, body_end):
 
 # Mapping dict from atom type code to handler function
 type_mapping = {
-    b'wide':process_wide,
-    b'skip':process_skip,
-    b'free':process_free,
-    b'ftyp':process_ftyp,
-    b'mdat':process_mdat,
-    b'moov':process_moov
+    b'wide':get_wide,
+    b'skip':get_skip,
+    b'free':get_free,
+    b'ftyp':get_ftyp,
+    b'mdat':get_mdat,
+    b'moov':get_moov
     }
 
 
-def process_next_atom(f, end_pos):
+def get_next_atom(f, end_pos):
     "read the next atom from file f and process it"
 
     pos = f.tell() # pos is the very start of the atom
@@ -118,7 +103,7 @@ def process_next_atom(f, end_pos):
         atom_size = int.from_bytes(data,"big") # convert 64 bits to integer
     
     # todo: handle special size cases, atom_size == 0
-    print("process_next_atom() data = ", data,
+    print("get_next_atom() data = ", data,
           "atom_size = ", atom_size,
           "atom_type = ", atom_type)
 
@@ -130,11 +115,42 @@ def process_next_atom(f, end_pos):
         # not enough remaining bytes for the atom body
         return None
 
-    atom_handler = type_mapping.get(atom_type, process_unknown)
+    atom_handler = type_mapping.get(atom_type, get_unknown)
     atom_body = atom_handler(f, body_end)
 
     return [atom_type, atom_size, atom_body]
+
+
+def is_valid_file(atom):
+    "check whether atom represents a valid QuickTime file"
+
+    if (None == atom):
+        print("No atom found")
+        return False
+
+    if (atom[0] != b'ftyp'):
+        print("No ftyp atom found, first atom = ", atom)
+        return False
+
+    type_list = atom[2]
+    major_brand = type_list[0]
     
+    if (major_brand == b'isom'):
+        print('It is an ISO Base Media file (MPEG-4) v1')
+    elif (major_brand == b'MSNV'):
+        print('It is a MPEG-4 video file')
+    elif (major_brand == b'mp42'):
+        print('It is an MPEG-4 video|QuickTime file')
+    elif (major_brand == b'qt  '):
+        print('It is a QuickTime movie file')
+    elif (major_brand == b'M4V '):
+        print('It is an ISO Media, MPEG v4 system, or iTunes AVC-LC file')
+    else:
+        print('Unknown ftyp', major_brand)
+        return False
+
+    return True
+
 
 file_list = sys.argv        # get the argument list
 prog_name = file_list.pop(0) # pop the script name off the head of the list
@@ -144,17 +160,24 @@ print("file_list = ", file_list)
 
 # Loop over the files from the command line
 for pathname in file_list:
-    atom_list = []
-    file_length = os.stat(pathname).st_size
     print("pathname = ", pathname)
+    file_length = os.stat(pathname).st_size
+    
     with open(pathname, 'rb') as f:
-        # Loop over the atoms in the file
-        while (f.tell() < file_length):
-            atom_list.append (process_next_atom(f, file_length))
-#            print("file offset = ", f.tell())
+        # Get the first atom in the file
+        atom_list = get_next_atom(f, file_length)
 
-    # We've been through the file, now print the list of atoms
-    print("Atom_list = ", atom_list)
+        # check if the first atom is a valid ftyp
+        if (is_valid_file(atom_list)):
+            # Loop over the remaining atoms in the file
+            while (f.tell() < file_length):
+                atom_list.append (get_next_atom(f, file_length))
+                print("file offset = ", f.tell())
+        else:
+            print("Invalid QuickTime file")
+            
+        # We've been through the file, now print the list of atoms
+        print("atom_list = ", atom_list)
     
         
 
